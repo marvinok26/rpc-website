a192df5f73fe15a588e85b939aa7cf9146170377
Marvin <okongomarvin971@gmail.com>
2024-08-01 17:08:55 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,83 +1,73 @@
 import React from 'react';
-import '../styles/Footer.css';
-import { FaFacebookF, FaInstagram, FaTwitter, FaGoogle, FaYoutube, FaLinkedinIn, FaTiktok } from 'react-icons/fa';
-import { MdLocationOn, MdPhone, MdEmail } from 'react-icons/md';
+
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
 
 import logo from '../Assets/Images/Logo/RPC_LTD_LOGO-removebg-preview.png';
+import '../styles/Footer.css';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-  
-      <div className="subscribe-section">
-        <h2>Subscribe to our emails</h2>
-        <div className="subscribe-form">
-          <input type="email" placeholder="Enter your email" />
-          <button type="submit">Subscribe</button>
+    <footer>
+      <div className="content">
+        <div className="top">
+          <div className="logo-details">
+            <Link to="/" className="logo-container">
+              <img src={logo} alt="Logo" className="logo" />
+              <h5 className='logo-text'>Real Plan Consultants Ltd</h5>
+            </Link>
+          </div>
+          <div className="media-icons">
+            <a href="www.facebook.com" aria-label="Facebook"><FaFacebookF /></a>
+            <a href="www.twitter.com" aria-label="Twitter"><FaTwitter /></a>
+            <a href="www.instagram.com" aria-label="Instagram"><FaInstagram /></a>
+            <a href="www.linkedin.com" aria-label="LinkedIn"><FaLinkedinIn /></a>
+            <a href="www.youtube.com" aria-label="YouTube"><FaYoutube /></a>
+          </div>
+        </div>
+        <div className="link-boxes">
+          <ul className="box">
+            <li className="link_name">Company</li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/services">Our Services</a></li>
+            <li><a href="/about">About us</a></li>
+            <li><a href="/projects">Projects</a></li>
+          </ul>
+          <ul className="box">
+            <li className="link_name">Services</li>
+            <li><a href="/services">GIS & Mapping</a></li>
+            <li><a href="/services">Physical & Land use Planning</a></li>
+            <li><a href="/services">Development Applications</a></li>
+            <li><a href="/services">Environmental Services</a></li>
+          </ul>
+          <ul className="box">
+            <li className="link_name">Opening Hours</li>
+            <li>Monday to Friday</li>
+            <li> 8.00 am to 5.00 pm</li>
+          </ul>
+          <ul className="box">
+            <li className="link_name">Contact us</li>
+            <li>020-2214116</li>
+            <li>0722477500</li>
+            <li>info@realplan.co.ke</li>
+            
+          </ul>
+          <ul className="box input-box">
+            <li className="link_name">Subscribe</li>
+            <li><input type="text" placeholder="Enter your email" /></li>
+            <li><input type="button" value="Subscribe" /></li>
+          </ul>
         </div>
       </div>
-      
-      <div className="footer-content">
-        <div className="footer-left">
-
-          <Link to="/" className="logo-container">
-            <img src={logo} alt="Logo" className="logo" />
-            <h5 className='logo-text'>Real Plan Consultants Ltd</h5>
-          </Link>
-
-          <div className="location-details">
-            <MdLocationOn className="icon" />
-            <p>4th Avenue Towers, <br />
-              Along 4th Ngong Avenue <br />
-              Upper Hill, Nairobi</p>
-          </div>
-          <div className="social-media">
-            <a href="https://facebook.com" aria-label="Facebook"><FaFacebookF /></a>
-            <a href="https://instagram.com" aria-label="Instagram"><FaInstagram /></a>
-            <a href="https://twitter.com" aria-label="X"><FaTwitter /></a>
-            <a href="mailto:example@gmail.com" aria-label="Gmail"><FaGoogle /></a>
-            <a href="https://youtube.com" aria-label="YouTube"><FaYoutube /></a>
-            <a href="https://linkedin.com" aria-label="LinkedIn"><FaLinkedinIn /></a>
-            <a href="https://tiktok.com" aria-label="TikTok"><FaTiktok /></a>
-          </div>
-        </div>
-
-        <div className="footer-center">
-          <div className="column">
-            <h3>Pages</h3>
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About Us</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/projects">Projects</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
-          </div>
-          <div className="column">
-            <h3>Services</h3>
-            <ul>
-              <li>Service 1</li>
-              <li>Service 2</li>
-              <li>Service 3</li>
-              <li>Service 4</li>
-            </ul>
-          </div>
-        </div>
-        
-        <div className="footer-right">
-          <div className="contact-info">
-            <MdPhone className="icon" />
-            <p>(123) 456-7890</p>
-          </div>
-          <div className="contact-info">
-            <MdEmail className="icon" />
-            <p>info@example.com</p>
-          </div>
-          <div className="contact-info">
-            <p>Opening Hours:</p>
-            <p>Mon-Fri: 9 AM - 5 PM</p>
-          </div>
+      <div className="bottom-details">
+        <div className="bottom_text">
+          <span className="copyright_text">
+            Copyright © 2024 <a href="/">arvia_codes</a> All rights reserved
+          </span>
+          <span className="policy_terms">
+            <a href="/">Privacy policy</a>
+            <a href="/">Terms & condition</a>
+          </span>
         </div>
       </div>
     </footer>
