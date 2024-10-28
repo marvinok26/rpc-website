ce8a6dc92fc84310f711a28eead297044ba204eb
marvin <okongomarvin971@gmail.com>
2024-10-28 23:54:24 +0300

    projects update

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -9,6 +9,8 @@ const Clients = () => {
   return (
     <>
       <ImageRow />
+
+
       <h1 className="text-3xl mt-8 mx-8 text-blue-700 text-center sm:text-2xl sm:mt-6 sm:mx-4">OUR CLIENTS</h1>
       <div className="p-5 sm:p-3">
         {categories.map((category, index) => (
@@ -18,7 +20,32 @@ const Clients = () => {
             images={category.images}
           />
         ))}
+
       </div>
+
+      <div class="bg-gray-100 px-4 py-12 font-sans">
+      <div class="max-w-4xl max-sm:max-w-sm mx-auto">
+        <h2 class="text-gray-800 text-4xl max-sm:text-2xl font-extrabold mb-8">Application Metrics</h2>
+        <div class="grid md:grid-cols-4 sm:grid-cols-2 gap-5">
+          <div class="bg-white rounded-xl border px-7 py-8">
+            <p class="text-gray-400 text-base font-semibold mb-1">Total free services</p>
+            <h3 class="text-blue-600 text-3xl font-extrabold">5.4M+</h3>
+          </div>
+          <div class="bg-white rounded-xl border px-7 py-8">
+            <p class="text-gray-400 text-base font-semibold mb-1">Revenue a month</p>
+            <h3 class="text-blue-600 text-3xl font-extrabold">$80K</h3>
+          </div>
+          <div class="bg-white rounded-xl border px-7 py-8">
+            <p class="text-gray-400 text-base font-semibold mb-1">Engagement</p>
+            <h3 class="text-blue-600 text-3xl font-extrabold">100K</h3>
+          </div>
+          <div class="bg-white rounded-xl border px-7 py-8">
+            <p class="text-gray-400 text-base font-semibold mb-1">Server Uptime</p>
+            <h3 class="text-blue-600 text-3xl font-extrabold">99.9%</h3>
+          </div>
+        </div>
+      </div>
+    </div>
 
       {/* Structured Data for SEO */}
       <script type="application/ld+json">
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { projectDetails } from '../data/projectDetailData';
 import { Helmet } from 'react-helmet';
 import { FaUser, FaClock, FaFolder, FaClipboard } from 'react-icons/fa';
+import bannerimg from '../Assets/PROJECTS BATCH 1/COUNTY SPATIAL PLAN/Agro_Ecological Zones_ AUGUST 2021.webp'
 
 const ProjectDetail = () => {
   return (
@@ -15,7 +16,17 @@ const ProjectDetail = () => {
         />
       </Helmet>
 
-      <div className="project-detail-container mt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
+      <div class="relative font-sans before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10 mt-[5rem]">
+  <img src={bannerimg} alt="Banner Image" class="absolute inset-0 w-full h-full object-cover" />
+
+  <div class="h-[300px] relative z-50 max-w-6xl mx-auto flex flex-col justify-center items-start text-left text-white p-6">
+    <h1 class="text-10xl sm:text-5xl font-extrabold mb-6">Projects</h1>
+  </div>
+</div>
+
+
+
+      <div className="project-detail-container mt-0 grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
         {Object.keys(projectDetails).map((projectId, index) => {
           const project = projectDetails[projectId];
           const isRightColumn = index % 2 !== 0;
@@ -40,14 +51,14 @@ const ProjectDetail = () => {
                   alt={project.title}
                   className="w-full h-[300px] object-contain"
                 />
-                <div className="absolute top-4 left-4 bg-yellow-400 text-black px-4 py-1 rounded">
+                <div className="absolute bottom-4 right-4 bg-yellow-400 text-black px-4 py-1 rounded">
                   {project.title.toUpperCase()}
                 </div>
               </div>
 
               {/* Project Info */}
               <div className="p-6">
-                
+
                 <p className="text-gray-700 mb-6">
                   {project.description || "This is a demo description for the project category."}
                 </p>
